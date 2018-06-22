993b4f05d625f1b7cdb2786bbdc845f2a5bbc582
Pavan Keerthi <pavan.keerthi@gmail.com>
2018-06-22 17:38:02 +0100

    adding logic in entrypoint for debugging

--- airflow/webui/startup.sh
+++ airflow/webui/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Run once, hold otherwise
+if [ -f "already_ran" ]; then
+    echo "Already ran the Entrypoint once. Holding indefinitely for debugging."
+    /bin/sh
+fi
+touch already_ran
+
+
+# Do your main things down here
 mkdir -p $AIRFLOW_DAG
 mkdir -p $AIRFLOW_LOG
 
