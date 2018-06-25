878c6792914e0426301ce8d45edfd3da1bd5187d
Pavan Keerthi <pavan.keerthi@gmail.com>
2018-06-25 14:47:58 +0100

    airflow webui entrypoint script

--- airflow/webui/startup.sh
+++ airflow/webui/startup.sh
@@ -13,7 +13,13 @@ touch already_ran
 mkdir -p $AIRFLOW_DAG
 mkdir -p $AIRFLOW_LOG
 
-if [ "$1" = 'webserver' ]; then
-    airflow initdb && airflow webserver
+if [ "$1" = "webserver" ]; then
+    sleep 10
+    echo "Initializing airflow db" 
+    airflow initdb
+    echo "sleep for 10 secs"
+    sleep 10
+    echo "Initializing airflow webserver"
+    airflow webserver
 fi
 exec "$@"
