d9224b50650e030e196f11c1c955f0e326ff6260
Pavan Keerthi <pavan.keerthi@gmail.com>
2018-06-25 14:11:14 +0100

    checking line ending and set option in shell script

--- airflow/webui/startup.sh
+++ airflow/webui/startup.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
+set -e
+
 # Run once, hold otherwise
 if [ -f "already_ran" ]; then
     echo "Already ran the Entrypoint once. Holding indefinitely for debugging."
     cat
 fi
 touch already_ran
-
 
 # Do your main things down here
 mkdir -p $AIRFLOW_DAG
