fd19f9b24a4794fc60a79624fbcc624ee2531dd7
Pavan Keerthi <pavan.keerthi@gmail.com>
2018-06-26 15:26:08 +0100

    refactored airflow codebase

--- airflow/webui/startup.sh
+++ airflow/webui/startup.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-set -e
-
-# Run once, hold otherwise
-if [ -f "already_ran" ]; then
-    echo "Already ran the Entrypoint once. Holding indefinitely for debugging."
-    cat
-fi
-touch already_ran
-
-# Do your main things down here
-mkdir -p $AIRFLOW_DAG
-mkdir -p $AIRFLOW_LOG
-
-airflow initdb
-airflow webserver
+case "$1" in
+  webserver)
+    airflow initdb
+    exec airflow webserver
+    ;;
+  worker|scheduler)
+    # To give the webserver time to run initdb.
+    sleep 10
+    exec airflow "$@"
+    ;;
+  flower)
+    exec airflow "$@"
+    ;;
+  version)
+    exec airflow "$@"
+    ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec "$@"
+    ;;
+esac
